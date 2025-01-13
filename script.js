473a9d4e4565d1add0c6a61f62fe2d58ab5c7aa9
SasakaRoben <sasakarobenld@gmail.com>
2025-01-13 06:30:34 +0300

    Add hover effect
    Used event delegation for more optimized and efficient code performance

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ for (let i = 0; i < 16 * 16; i++)
     square.classList.add('grid-square');
     container.appendChild(square);
 }
+
+container.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('grid-square')) {
+        event.target.style.backgroundColor = 'yellow';
+    }
+});
+
+container.addEventListener('mouseout', (event) => {
+    if (event.target.classList.contains('grid-square')) {
+        event.target.style.backgroundColor = 'white';
+    }
+});
+
+
+
